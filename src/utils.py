f41cacf2aed3791755a29b942acdd4254f2099cb
Don Fox <dfox09@gmail.com>
2023-05-18 17:38:34 -0400

    Correct preprocess based on testing

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import re
 
 def preprocess(word):
     try:
-        re.search(r"([a-z]+)" word.lower()).group(1)
+        return re.search(r"([a-z]+)", word.lower()).group(1)
     except AttributeError:
         return None
 
